32ed910bdf6ac1b0c23437aa74c090cb46d8d100
Shubham Saxena <shubh3695@gmail.com>
2018-07-26 22:37:40 +0530

    e2-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  name : string;
+  artists : ArtistModel[];
+  constructor(){
+    this.name = "Artists";
+    this.artists = [ {
+      name: "Shubham Kumar", 
+      school: "Penn State"
+    },
+    {
+      name: "Shubham Saxena", 
+      school: "Penn State"
+    }, 
+    {
+      name: "Shubham Singh", 
+      school: "Penn State"
+    }
+      ];
+  }
+  btnClick(value : string){
+      console.log(value);
+      this.artists.push({
+        name : value, 
+        school: "Penn State"
+      });
+  }
+  clickEvent(event) : void{
+    this.name = (event.target.innerHTML);
+  }
+}
+interface ArtistModel {
+  name : string;
+  school : string;
 }
